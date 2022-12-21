b9b7ba7675685239b335e8ba558d423ebb0eedac
Khushbu Rajgor <khushbu.rajgor@bacancy.com>
2022-12-21 16:17:24 +0530

    Added Project crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
+  # resources :comments
+  # resources :tickets
+  resources :projects
+  devise_for :users, controllers: { 
+    registrations: 'users/registrations', 
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
+  }
+  
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "users#index"
+  root  "projects#index" 
 end
